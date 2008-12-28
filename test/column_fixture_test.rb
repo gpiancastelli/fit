146e4a1b5317161ae42b6be96e421f74e68c1df1
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2008-12-28 10:51:21 +0100

    Added tests for ColumnFixture, d'oh

---
+++ test/column_fixture_test.rb
@@ -0,0 +1,28 @@
+# Copyright (c) 2002 Cunningham & Cunningham, Inc.
+# Released under the terms of the GNU General Public License version 2 or later.
+
+require 'test/unit'
+# Make the test run location independent
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+require 'fit/column_fixture'
+
+module Fit
+
+  class ColumnFixtureTest < Test::Unit::TestCase
+
+    # part of RubyForge bug #22283
+    def test_camel
+      fixture = TestColumnFixture.new
+      header_text = "Execution Data Value"
+      assert_equal "execution_data_value", fixture.camel(header_text, '')
+    end
+
+    class TestColumnFixture < ColumnFixture
+      def camel name, suffix
+        super name, suffix
+      end
+    end
+
+  end
+
+end
